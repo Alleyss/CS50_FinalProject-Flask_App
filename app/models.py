0d138b6b6b75b14525592cde14367f32d769b7be
Sai Pranav <vsaipranav2004@gmail.com>
2024-07-13 23:38:46 +0530

    Updating models.py

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class Student(db.Model):
     address = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
-
+    major = db.Column(db.String(128), nullable=False)
 class Admin(db.Model):
     __tablename__ = 'admins'
     id = db.Column(db.Integer, primary_key=True)
