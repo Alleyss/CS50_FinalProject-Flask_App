97c7dc93f3f2d548af4b3625921b147e6e502889
Sai Pranav <vsaipranav2004@gmail.com>
2024-07-24 11:09:24 +0530

    final commit 5.html

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'pranav'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:vsp2004@localhost:5432/school'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://school_k16a_user:6kn90FWVuX5yv8rLfZpf4RtWA6Nv4CxS@dpg-cqfvftt6l47c73bmq9gg-a.oregon-postgres.render.com/school_k16a'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
