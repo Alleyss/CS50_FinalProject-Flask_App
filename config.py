ceb4e7dfe600b0e7a2e60d6de5ee5d0f6d9a1c32
Sai Pranav <vsaipranav2004@gmail.com>
2024-10-22 16:28:27 +0530

    changed the database_uri to local database_uri

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'pranav'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://schooldb_khj2_user:VwFQq2QYRRPuadc55Vvam1mWWGohRaCj@dpg-crdg5qd2ng1s73fuirkg-a.oregon-postgres.render.com/schooldb_khj2'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:vsp2004@localhost:5432/student_portal_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
